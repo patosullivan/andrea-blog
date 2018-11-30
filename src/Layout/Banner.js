f2e830d60297de0385c89776f0e657ac198be153
Patrick O'Sullivan <posullivan@uwf.edu>
2018-11-30 15:24:13 -0600

    Updated banner image and style

--- src/Layout/Banner.js
+++ src/Layout/Banner.js
@@ -2,16 +2,16 @@ import React from "react"
 import { Jumbotron } from "reactstrap"
 
 const jumboStyle = {
-  backgroundImage: "url(./foggyMountains.jpg)",
+  backgroundImage: "url(./angularBuilding.jpg)",
   backgroundAttachment: "fixed",
-  backgroundPosition: "center",
+  backgroundPosition: "bottom",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover"
 }
 
 const Banner = () => (
-  <Jumbotron className="my-2 text-center" style={jumboStyle}>
-    <h1 className="display-4 text-monospace">{"Andrea O'Sullivan"}</h1>
+  <Jumbotron className="my-2 text-center text-white" style={jumboStyle}>
+    <h1 className="display-4 text-console">{"Andrea O'Sullivan"}</h1>
     <p className="lead text-monospace">Technology and Public Policy</p>
   </Jumbotron>
 )
