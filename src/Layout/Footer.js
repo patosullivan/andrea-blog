03731aaf80caabcab7f330c48f43b5b57c0da3c4
Patrick <patrick@osullivan.io>
2018-12-02 21:08:09 -0600

    removed extraneous pipe

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -4,7 +4,7 @@ const year = new Date().getFullYear()
 
 const Footer = () => (
   <footer className="bg-light border w-100 mx-0 px-2 py-1 mt-2">
-    <small className="text-muted">&copy; {year} | </small>
+    <small className="text-muted">&copy; {year} </small>
   </footer>
 )
 
