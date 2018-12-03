737fe0a91e5d74b64b8b536fffee6957a0de82c0
Patrick <patrick@osullivan.io>
2018-12-02 20:49:06 -0600

    removed border radius

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,17 +1,16 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faGithub} from "@fortawesome/free-brands-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import React from "react"
 
 const year = new Date().getFullYear()
 
 const Footer = () => (
-  <footer className="bg-light border w-100 mx-0 px-2 py-1 mt-2 rounded">
+  <footer className="bg-light border w-100 mx-0 px-2 py-1 mt-2">
     <small className="text-muted">
       &copy; {year} |{" "}
       <a
         className="text-muted"
-        href="https://github.com/patosullivan/personal-website"
-      >
+        href="https://github.com/patosullivan/personal-website">
         Check out the repo for this web app on{" "}
         <FontAwesomeIcon icon={faGithub} />
       </a>
