5abfac06f6450e45f073fdd5cac61fa2c1c5e46c
Patrick <patrick@osullivan.io>
2018-12-02 20:51:58 -0600

    updated About page

--- src/Content/About.js
+++ src/Content/About.js
@@ -1,27 +1,33 @@
-import family from "./family.jpg"
+import andrea from "./andrea.jpg"
 import React from "react"
-import { Col, Row } from "reactstrap"
+import {Col, Row} from "reactstrap"
 
 const About = () => (
   <Row>
     <Col>
-      <img src={family} className="w-100 border rounded float-left" />
+      <img src={andrea} className="w-75 rounded float-right" />
     </Col>
     <Col sm>
       <br />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium
-        efficitur rutrum. Pellentesque augue neque, posuere sit amet elementum
-        tempor, vestibulum ac tortor. Aliquam nec nisl in odio sagittis lacinia.
-        Vivamus turpis mauris, consequat a orci quis, efficitur bibendum orci.
-        Cras non posuere mauris. Donec vel congue turpis.
-      </p>
-      <p>
-        Suspendisse a sapien lectus. Aenean nec nunc elit. Integer hendrerit
-        tempus tempor. Nam luctus purus libero, eu pretium orci mollis in.
-        Nullam ultricies volutpat pharetra. Cras tincidunt ex vel erat commodo
-        feugiat. Fusce sagittis tincidunt urna, ut ornare purus gravida eget.
-        Integer tempor nisi vel lacus aliquam placerat.
+        {"Andrea O'Sullivan"} is a feature writer for The Bridge at the Mercatus
+        Center at George Mason University. Her work focuses on cybersecurity,
+        surveillance, Internet freedom, cryptocurrency, and the economics of
+        technology. Her writing has been published in the{" "}
+        <em>
+          Wall Street Journal, Chicago Tribune, Newsweek, Washington Times, US
+          News and World Reports,
+        </em>
+        and other outlets, and she pens a regular column on technology topics
+        for <em>Reason</em>. She was selected as one of Forbes’ 30 Under 30 in
+        policy in 2016. She is a coauthor of{" "}
+        <em>
+          Liberalism and Cronyism: Two Rival Political and Economic Systems
+        </em>{" "}
+        with Randall G. Holcombe and <em>Bitcoin: A Primer for Policymakers</em>{" "}
+        with Jerry Brito. {"O'Sullivan"} studied economics and political science
+        at Florida State University on the undergraduate level and economics at
+        George Mason University at the graduate level.
       </p>
     </Col>
   </Row>
