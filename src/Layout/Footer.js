d0fa7a9e92c381f59ac7c10133a5a5d00c001099
Patrick <patrick@osullivan.io>
2018-12-02 21:04:51 -0600

    removed linked to github from footer

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,21 +1,10 @@
-import {faGithub} from "@fortawesome/free-brands-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import React from "react"
 
 const year = new Date().getFullYear()
 
 const Footer = () => (
   <footer className="bg-light border w-100 mx-0 px-2 py-1 mt-2">
-    <small className="text-muted">
-      &copy; {year} |{" "}
-      <a
-        className="text-muted"
-        href="https://github.com/patosullivan/personal-website">
-        Check out the repo for this web app on{" "}
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-      .
-    </small>
+    <small className="text-muted">&copy; {year} | </small>
   </footer>
 )
 
