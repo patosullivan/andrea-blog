d5f7055b5e0f3429ea4845a7b9ce30cb6ac2117d
Patrick O'Sullivan <posullivan@uwf.edu>
2018-12-03 12:59:57 -0600

    increased image compression

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
       test: /\.(jpe?g|png|gif|svg)$/i,
       plugins: [
         imageminMozjpeg({
-          quality: 80,
+          quality: 30,
           progessive: true
         })
       ]
