8edf0e3c06509919273530be201cbe44afbbbffd
Patrick <patrick@osullivan.io>
2018-12-02 19:44:51 -0600

    changed font in the banner

--- src/Layout/Banner.js
+++ src/Layout/Banner.js
@@ -1,17 +1,18 @@
 import React from "react"
-import { Jumbotron } from "reactstrap"
+import {Jumbotron} from "reactstrap"
 
 const jumboStyle = {
   backgroundImage: "url(./angularBuilding.jpg)",
   backgroundAttachment: "fixed",
   backgroundPosition: "bottom",
   backgroundRepeat: "no-repeat",
-  backgroundSize: "cover"
+  backgroundSize: "cover",
+  fontFamily: "'Merriweather', serif"
 }
 
 const Banner = () => (
   <Jumbotron className="my-2 text-center text-white" style={jumboStyle}>
-    <h1 className="display-4 text-console">{"Andrea O'Sullivan"}</h1>
+    <h1 className="display-4">{"Andrea O'Sullivan"}</h1>
     <p className="lead text-monospace">Technology and Public Policy</p>
   </Jumbotron>
 )
