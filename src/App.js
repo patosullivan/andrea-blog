a1803a86619afbfc9903ad7b02b22474bb7a1cf1
Patrick <patrick@osullivan.io>
2018-12-02 20:44:51 -0600

    rearranged the page order

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Banner from "./Layout/Banner"
 import Footer from "./Layout/Footer"
 import Main from "./Layout/Main"
 import Nav from "./Layout/Nav"
-import React, { useState } from "react"
-import { Col, Container, Row } from "reactstrap"
+import React, {useState} from "react"
+import {Col, Container, Row} from "reactstrap"
 
 const App = () => {
-  let [page, setPage] = useState("Writing")
+  let [page, setPage] = useState("About")
 
-  const pages = ["Writing", "About", "Contact"]
+  const pages = ["About", "Writing", "Contact"]
 
   return (
     <Container>
