37a309e82206c94b3fd6158086c47ad614e486ba
Patrick <patrick@osullivan.io>
2018-12-03 23:26:25 -0600

    got initial blog up

--- src/Content/Writing/Writing.js
+++ src/Content/Writing/Writing.js
@@ -1,5 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import PostCard from "./PostCard.js"
 
-const Writing = () => <p>Blog content will go here.</p>
+const Writing = () => {
+  let [posts, setPosts] = useState([])
+
+  //  let [postCount, setPostCount] = useState(0)
+
+  useEffect(() => {
+    fetch("http://68.183.57.61:1337/posts_limit=5")
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+
+  // useEffect(() => {
+  //  fetch("http://68.183.57.61:1337/posts/count")
+  //    .then(res => res.text())
+  //    .then(text => setPostCount(text))
+  // }, [])
+
+  return (
+    <>
+      {posts.map(post => (
+        <PostCard key={post.id} body={post.content} title={post.title} />
+      ))}
+    </>
+  )
+}
 
 export default Writing
