1a6baede55057dc777739096a60f280366a05c67
Patrick O'Sullivan <posullivan@uwf.edu>
2018-12-04 16:53:14 -0600

    now fetching from proper url. added a show more button.

--- src/Content/Writing/Writing.js
+++ src/Content/Writing/Writing.js
@@ -1,28 +1,29 @@
 import React, { useState, useEffect } from "react"
 import PostCard from "./PostCard.js"
-
+import { Button } from "reactstrap"
 const Writing = () => {
   let [posts, setPosts] = useState([])
 
-  //  let [postCount, setPostCount] = useState(0)
+  let [postCount, setPostCount] = useState(0)
 
   useEffect(() => {
-    fetch("http://68.183.57.61:1337/posts_limit=5")
+    fetch("http://cms.andrea.osullivan.io:1337/posts?_limit=5")
       .then(res => res.json())
       .then(data => setPosts(data))
   }, [])
 
-  // useEffect(() => {
-  //  fetch("http://68.183.57.61:1337/posts/count")
-  //    .then(res => res.text())
-  //    .then(text => setPostCount(text))
-  // }, [])
+  useEffect(() => {
+    fetch("http://cms.andrea.osullivan.io:1337/posts/count")
+      .then(res => res.text())
+      .then(text => setPostCount(text))
+  }, [])
 
   return (
     <>
       {posts.map(post => (
         <PostCard key={post.id} body={post.content} title={post.title} />
       ))}
+      {postCount > 5 ? <Button>Show more posts</Button> : null}
     </>
   )
 }
