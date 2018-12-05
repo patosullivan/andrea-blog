921d0edbdc96be0dd8c909d23e735008730cb2f3
Patrick <patrick@osullivan.io>
2018-12-04 22:30:26 -0600

    updated prop names, moved date, formatted date

--- src/Content/Writing/PostCard.js
+++ src/Content/Writing/PostCard.js
@@ -1,24 +1,26 @@
 import ProptTypes from "prop-types"
 import React from "react"
-import {Card, CardHeader, CardTitle, CardBody, CardFooter} from "reactstrap"
+import { Card, CardHeader, CardTitle, CardBody } from "reactstrap"
 import ReactMarkdown from "react-markdown"
 
 const PostCard = props => (
-  <Card>
+  <Card className="my-2">
     <CardHeader>
       <CardTitle>{props.title}</CardTitle>
+      <small>
+        <em>{props.createdAt}</em>
+      </small>
     </CardHeader>
     <CardBody>
-      <ReactMarkdown source={props.body} />
+      <ReactMarkdown source={props.content} />
     </CardBody>
-    <CardFooter>{props.date}</CardFooter>
   </Card>
 )
 
 PostCard.propTypes = {
   title: ProptTypes.string,
-  body: ProptTypes.string,
-  date: ProptTypes.string
+  content: ProptTypes.string,
+  createdAt: ProptTypes.string
 }
 
 export default PostCard
