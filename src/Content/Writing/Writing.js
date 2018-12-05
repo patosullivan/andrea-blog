6118a259405fe6b05f1f63de1930be2127f77dd8
Patrick <patrick@osullivan.io>
2018-12-04 22:32:33 -0600

    formatted date with dayjs, got the show more button working, updated api URL. blog now works.

--- src/Content/Writing/Writing.js
+++ src/Content/Writing/Writing.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react"
 import PostCard from "./PostCard.js"
 import { Button } from "reactstrap"
+import dayjs from "dayjs"
+
 const Writing = () => {
   let [posts, setPosts] = useState([])
 
   let [postCount, setPostCount] = useState(0)
 
-  useEffect(() => {
-    fetch("http://cms.andrea.osullivan.io:1337/posts?_limit=5")
-      .then(res => res.json())
-      .then(data => setPosts(data))
-  }, [])
+  let [showMoreClicks, setShowMoreClicks] = useState(0)
+
+  useEffect(
+    () => {
+      const postURL = `https://cms.andrea.osullivan.io/posts?_limit=${5 *
+        (showMoreClicks + 1)}&_sort=createdAt:desc`
+      fetch(postURL)
+        .then(res => res.json())
+        .then(data => setPosts(data))
+    },
+    [showMoreClicks]
+  )
 
   useEffect(() => {
-    fetch("http://cms.andrea.osullivan.io:1337/posts/count")
+    fetch("https://cms.andrea.osullivan.io/posts/count")
       .then(res => res.text())
       .then(text => setPostCount(text))
   }, [])
@@ -21,9 +30,18 @@ const Writing = () => {
   return (
     <>
       {posts.map(post => (
-        <PostCard key={post.id} body={post.content} title={post.title} />
+        <PostCard
+          key={post.id}
+          content={post.content}
+          title={post.title}
+          createdAt={dayjs(post.createdAt).format("dddd, MMMM D, YYYY ")}
+        />
       ))}
-      {postCount > 5 ? <Button>Show more posts</Button> : null}
+      {postCount > 5 ? (
+        <Button onClick={() => setShowMoreClicks(showMoreClicks + 1)}>
+          Show more posts
+        </Button>
+      ) : null}
     </>
   )
 }
