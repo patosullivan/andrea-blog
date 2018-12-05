2c9543677c74eb69963fb52af9e7f9b5f0dfed00
Patrick <patrick@osullivan.io>
2018-12-04 22:31:15 -0600

    added correct contact info

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -1,24 +1,13 @@
 import React from "react"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ListGroup, ListGroupItem } from "reactstrap"
 
 const Contact = () => (
   <ListGroup>
     <ListGroupItem>
-      <a href="mailto:patrick@osullivan.io">
+      <a href="mailto:andrea@osullivan.io">
         <FontAwesomeIcon icon={faEnvelope} /> Email
-      </a>
-    </ListGroupItem>
-    <ListGroupItem>
-      <a href="https://github.com/patosullivan">
-        <FontAwesomeIcon icon={faGithub} /> GitHub
-      </a>
-    </ListGroupItem>
-    <ListGroupItem>
-      <a href="https://linkedin.com/in/patosullivan">
-        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
       </a>
     </ListGroupItem>
   </ListGroup>
