d7764169907419993f4d9f3d07a73322d1bc996c
Patrick O'Sullivan <posullivan@uwf.edu>
2018-12-05 13:15:49 -0600

    added twitter

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ListGroup, ListGroupItem } from "reactstrap"
 
@@ -8,6 +9,11 @@ const Contact = () => (
     <ListGroupItem>
       <a href="mailto:andrea@osullivan.io">
         <FontAwesomeIcon icon={faEnvelope} /> Email
+      </a>
+    </ListGroupItem>
+    <ListGroupItem>
+      <a href="https://twitter.com/anjiecast">
+        <FontAwesomeIcon icon={faTwitter} /> @anjiecast
       </a>
     </ListGroupItem>
   </ListGroup>
