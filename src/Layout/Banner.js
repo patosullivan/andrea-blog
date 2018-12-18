ae20201eb466cda0a866830f1b7f33a8ad0e89a6
Patrick <patrick@osullivan.io>
2018-12-18 11:30:15 -0600

    started using emotionjs

--- src/Layout/Banner.js
+++ src/Layout/Banner.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Jumbotron} from "reactstrap"
+import { Jumbotron } from "reactstrap"
 
 const jumboStyle = {
   backgroundImage: "url(./triangles.jpg)",
@@ -11,11 +11,9 @@ const jumboStyle = {
 }
 
 const Banner = () => (
-  <Jumbotron
-    className="my-2 text-center text-black rounded-0"
-    style={jumboStyle}>
+  <Jumbotron className="my-2 text-center text-black rounded-0" css={jumboStyle}>
     <h1 className="display-4">{"Andrea O'Sullivan"}</h1>
-    <p className="lead" style={{fontFamily: "'Montserrat', sans-serif"}}>
+    <p className="lead" style={{ fontFamily: "'Montserrat', sans-serif" }}>
       Technology and Public Policy
     </p>
   </Jumbotron>
