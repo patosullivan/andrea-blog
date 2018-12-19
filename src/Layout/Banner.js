5a5956f0deacb78db668a29a5073facb933e7e6a
Patrick <patrick@osullivan.io>
2018-12-19 15:19:09 -0600

    made some adjustments in style for smaller phones

--- src/Layout/Banner.js
+++ src/Layout/Banner.js
@@ -1,18 +1,35 @@
 import React from "react"
 import { Jumbotron } from "reactstrap"
+import { css } from "@emotion/core"
 
-const jumboStyle = {
-  backgroundImage: "url(./triangles.jpg)",
-  backgroundPosition: "bottom",
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  fontFamily: "'Merriweather', serif"
-}
+const jumboStyle = css`
+  background-image: url(./triangles.jpg);
+  background-position: bottom;
+  background-repeat: no-repeat;
+  background-size: cover;
+  fontFamily: 'Merriweather', serif";
+`
+
+const nameStyle = css`
+  @media (max-width: 375px) {
+    font-size: 3rem;
+    font-weight: 300;
+    line-height: 1.2;
+  }
+`
+
+const summaryStyle = css`
+  @media (max-width: 320px) {
+    font-size: 1rem;
+  }
+`
 
 const Banner = () => (
   <Jumbotron className="my-2 text-center text-black rounded-0" css={jumboStyle}>
-    <h1 className="display-4">{"Andrea O'Sullivan"}</h1>
-    <p className="lead" style={{ fontFamily: "'Montserrat', sans-serif" }}>
+    <h1 css={nameStyle} className="display-4">
+      {"Andrea O'Sullivan"}
+    </h1>
+    <p css={summaryStyle} className="lead">
       Technology and Public Policy
     </p>
   </Jumbotron>
